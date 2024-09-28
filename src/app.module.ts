a36d9ba44125d161f6c5d1dc7e33e0b2e1e760ee
Carlos161104 <canoja161104@mail.com>
2024-09-28 15:39:17 -0600

    no se que se modifico por que no hizo video el profe, pero se modifico algo, y no compila por lo mismo XD saludos

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EmployeesModule } from './employees/employees.module';
 import { ProductsModule } from './products/products.module';
+import { ConfigModule } from "@nestjs/config";
 import { ProvidersModule } from './providers/providers.module';
 import { LocationsModule } from './locations/locations.module';
 import { ManagersModule } from './managers/managers.module';
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,29 +5,38 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
 import * as bcrypt from 'bcrypt';
+import * as Jwt from 'jsonwebtoken';
+import { JwtService } from "@nestjs/jwt";
+import { LoginUserDto } from "./dto/login-user.dto";
+
 
 @Injectable()
 export class AuthService {
-    constructor(
-        @InjectRepository(User)
-        private userRepository: Repository<User>
-    ) { }
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+    private jwtService: JwtService,
+  ) { }
+  registerUser(createUserDto: CreateUserDto) {
+    createUserDto.userPassword = bcrypt.hashSync(createUserDto.userPassword, 5);
+    return this.userRepository.save(createUserDto);
+  }
 
-    registerUser(createUserDto: CreateUserDto) {
-        createUserDto.userPassword = bcrypt.hashSync(createUserDto.userPassword, 5)
-        return this.userRepository.save(createUserDto)
-        
-    }
-
-    async loginUser(createUserDto: CreateUserDto) {
-      const user = await this.userRepository.findOne({
-        where: {
-          userEmail: createUserDto.userEmail
-        }
-      })
-      const match = await bcrypt.compare(createUserDto.userPassword, user.userPassword)
-
-      if (!match) throw new UnauthorizedException('Invalid credentials')
-      return { message: 'Login successful' }
-    }
+  async loginUser(loginUserDto: LoginUserDto) {
+    const user = await this.userRepository.findOne({
+      where: {
+        userEmail: loginUserDto.userEmail,
+      },
+    });
+    const match = await bcrypt.compare(
+      loginUserDto.userPassword,
+      user.userPassword,
+    );
+    if (!match) throw new UnauthorizedException('Invalid credentials')
+    const payload = {
+      user: user.userEmail,
+      password: user.userPassword,
+    };
+    const token = this.jwtService.sign(payload);
+    return token;
+  }
 }
